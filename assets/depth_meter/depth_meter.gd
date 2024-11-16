35a0728051cf1a2c30729adeedc6f3cdf28c3c0d
YAGOTAGO <tiagocaetanodavies@gmail.com>
2024-11-16 14:15:49 -0500

    Depth Display

--- assets/depth_meter/depth_meter.gd
+++ assets/depth_meter/depth_meter.gd
@@ -1,15 +1,26 @@
 extends Node
 
+const MAX_AMOUNT = 10
+const MIN_AMOUNT = 1
+const START_DEPTH = 10000
+
 @onready var progress_shader_material: ShaderMaterial = %ProgressBar.material as ShaderMaterial
 @onready var label: Label = %Label
 
-var depth_num: int = 10000
+var depth_num: int = START_DEPTH
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	progress_shader_material.set_shader_parameter("progress", 0)
+	_update_display()
+	
+func _on_timer_timeout() -> void:
+	var amount: int = randi_range(MIN_AMOUNT, MAX_AMOUNT)
+	depth_num -= amount
+	_update_display()
+	
+func _update_display():
 	label.text = str(depth_num)
+	var percentage: float = 1 - (depth_num as float / START_DEPTH as float)
+	progress_shader_material.set_shader_parameter("progress", percentage)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
