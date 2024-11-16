8daeb5b500d92b27f68f8b99a9e9487982698592
Tiago Davies <tiagocaetanodavies@gmail.com>
2024-11-16 08:46:28 -0500

    Radar working

--- scripts/2D Scripts/radar.gd
+++ scripts/2D Scripts/radar.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var animation: AnimationPlayer = %AnimationPlayer
 @onready var radar_shader_material: ShaderMaterial = %BG.material as ShaderMaterial
 
+
 func activate() -> void:
 	radar_shader_material.set_shader_parameter("show_blip", true)
 
@@ -12,3 +13,7 @@ func fix() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation.play("radar_cycle")
+	activate()
+
+func _on_area_3d_clicked() -> void:
+	fix()
--- scripts/3D Scripts/3D_mouse_detection.gd
+++ scripts/3D Scripts/3D_mouse_detection.gd
@@ -3,6 +3,10 @@ extends Area3D
 signal clicked
 var mouse_inside: bool = false
 
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
 func _process(delta: float) -> void:
 	if mouse_inside and Input.is_action_just_pressed("click"):
 		clicked.emit()
