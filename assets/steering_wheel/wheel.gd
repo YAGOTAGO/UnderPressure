fb5e4d9558723f37218ec2773e1741268079fb1a
YAGOTAGO <tiagocaetanodavies@gmail.com>
2024-11-16 12:24:14 -0500

    Renames

--- assets/steering_wheel/wheel.gd
+++ assets/steering_wheel/wheel.gd
@@ -6,6 +6,12 @@ const MIN_ROTATION: int = -30
 @onready var wheel: Node3D = %Wheel
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
+var is_broken: bool = false
+
+func activate() -> void:
+	
+	pass
+
 func _on_left_area_clicked() -> void:
 	if !animation_player.is_playing():
 		animation_player.play("wheel_left")
