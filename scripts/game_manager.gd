465c449585984a8a2ac099b74fc368bb9ad12707
YAGOTAGO <tiagocaetanodavies@gmail.com>
2024-11-16 10:12:53 -0500

    Game Manager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,3 +5,24 @@ extends Node
 
 func start_game() -> void:
 	pass
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
+
+func _activate_components()->void:
+	#get non broken components
+	var non_broken_components = list_components.filter(func(c): return not c.is_broken)
+	
+	# Check if there are any non-broken components left.
+	if non_broken_components.empty():
+		print("All components are broken!")
+		return
+	
+	# Randomly select a component from the non-broken list.
+	var random_index = randi() % non_broken_components.size()
+	var selected_component = non_broken_components[random_index]
+	
+	# Set the selected component to broken.
+	selected_component.is_broken = true
+	selected_component.activate();
+	print("Component broken:", selected_component.name)
