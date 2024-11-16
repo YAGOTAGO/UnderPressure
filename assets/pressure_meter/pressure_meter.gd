f205e6fcde9be6023b2de88171fe225503b422bd
notred27 <mikereidy95@gmail.com>
2024-11-16 15:21:48 -0500

    Finished Meter

--- assets/pressure_meter/pressure_meter.gd
+++ assets/pressure_meter/pressure_meter.gd
@@ -1,38 +1,55 @@
 extends Node3D
 
 @onready var timer: Timer = %Timer
+@onready var countdown: Timer = %PressureCountdown
 @onready var needle: Node3D = %MeterNeedle
 @onready var animations: AnimationPlayer = %AnimationPlayer
-var angle:int = 0
 
+var angle:float = 0.0	# The current angle of the needle
 var MIN_ANGLE:int = 0
 var MAX_ANGLE:int = 90
 
+var scale_factor:int = 0;	# How fast the pressure gauge will go off (0 is static)
+var is_compromised:bool = false # If the gauge has hit max pressure
+
+func activate() -> void:
+	scale_factor = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#animations.get_animation("needle_low").loop = true
-	
-	#animations.play("needel_low")
-	timer.start()
-	pass
+	timer.start()	# Start the timer to move the needle
+
 
 func move_needle() -> void:
 	needle.rotation_degrees.z = 30 -angle
+	
+	if angle > 80 and !is_compromised:
+		#print("About to blow")
+		countdown.start()
+		is_compromised = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+var time_sum:float = 0.0
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-		animations.play("needel_low")
+	time_sum += delta
+	angle += sin(20 * time_sum)  # Rotate dynamically
+	angle = max(min(MAX_ANGLE, angle), MIN_ANGLE)
+	
+	if time_sum > 1000:
+		time_sum = 0.0
+	
+	move_needle()
 
 
 func _on_timer_timeout() -> void:
-	angle += randi_range(0,10)
-	print("Timer")
-	#angle += 10
+	angle += randi_range(0, scale_factor)
+	angle = max(min(MAX_ANGLE, angle), MIN_ANGLE)
 	move_needle()
-	
-	
+
+
+func _on_pressure_countdown_timeout() -> void:
+	print("Component has failed...")
+	is_compromised = false
+	angle = 0
+	scale_factor = 0
