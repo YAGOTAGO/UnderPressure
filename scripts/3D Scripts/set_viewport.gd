0d5d816431a8f0e753ca4a76e9aa9fc718e3dc1f
YAGOTAGO <tiagocaetanodavies@gmail.com>
2024-11-16 00:40:06 -0500

    MIsc

--- scripts/3D Scripts/set_viewport.gd
+++ scripts/3D Scripts/set_viewport.gd
@@ -2,11 +2,6 @@ extends MeshInstance3D
 
 @export var viewport: SubViewport
 
-func _ready():
-	
-	#viewport.ClearMode = viewport.ClearMode.CLEAR_MODE_ONCE
-	
-		
-	# Retrieve the texture and set it to the viewport quad.
+func _ready()-> void:
 	material_override.albedo_texture = viewport.get_texture()
 	
