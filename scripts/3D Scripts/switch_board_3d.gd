76fe8ad9371d2ec12b1e54c3198865b159f5e667
notred27 <mikereidy95@gmail.com>
2024-11-16 00:05:16 -0500

    3D Switchboard

---
+++ scripts/3D Scripts/switch_board_3d.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+@onready var switch0: Node3D = %Switch3D0
+@onready var switch1: Node3D = %Switch3D1
+@onready var switch2: Node3D = %Switch3D2
+@onready var switch3: Node3D = %Switch3D3
+@onready var switch4: Node3D = %Switch3D4
+@onready var switch5: Node3D = %Switch3D5
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
