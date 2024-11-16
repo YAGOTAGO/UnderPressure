678bcae3ded261d4f657588c669076fba414c39c
notred27 <mikereidy95@gmail.com>
2024-11-15 23:05:10 -0500

    Added button with a cover

--- scripts/pressure_gadge_script.gd
+++ scripts/pressure_gadge_script.gd
@@ -24,8 +24,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
+	
 
 func _on_h_slider_value_changed(value: float) -> void:
 	update_rotation(value)
