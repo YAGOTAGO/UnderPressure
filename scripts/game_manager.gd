a2657ea2ee9770e7c8fe570b65d5123fb6f2d0ff
YAGOTAGO <tiagocaetanodavies@gmail.com>
2024-11-16 22:04:10 -0500

    Main Menu

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,9 +2,10 @@ class_name GameManager
 extends Node
 
 @export var list_components: Array[Node3D]
+@onready var timer: Timer = %Timer
 
 func start_game() -> void:
-	pass
+	timer.start(0)
 
 func _on_timer_timeout() -> void:
 	_activate_components()
