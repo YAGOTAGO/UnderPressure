20872639ffb3c1e6dbc7a3bd177a024063a88537
notred27 <mikereidy95@gmail.com>
2024-11-17 03:30:37 -0500

    switches fixed

--- assets/switch_board/switch_board_3d.gd
+++ assets/switch_board/switch_board_3d.gd
@@ -2,24 +2,32 @@ extends Node3D
 
 signal component_failed
 @onready var timer:Timer = %Timer
-
+@onready var audio:AudioStreamPlayer3D = %AudioStreamPlayer3D
 @export var switchArray:Array[Node3D]
 var is_broken: bool = false
 
 
-
+func _ready():
+	# Automatically populate switchArray with children of the current node
+	for child in get_children():
+		if child is Node3D and child is not MeshInstance3D and child is not AudioStreamPlayer3D:
+			switchArray.append(child)
 
 func activate() -> void:
 	print("activate switchboard")
 	is_broken = true
 	timer.start()
-	$AudioStreamPlayer3D.play()
+	audio.play()
+	print(switchArray.is_empty())
 	
-	for switch:Node3D in switchArray:
-		if(randi_range(0,5) >= 3):
+	for switch in switchArray:
+		print("switch")
+		if randi_range(0,5) >= 3:
 			print("break")
 			switch.break_switch()
 		else:
+			print("break2")
+			
 			switch.set_correct()
 			
 			
